0fc14603cbbe7345fb46022da3d4a3c06b83e103
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-26 22:41:52 +0530

    changes for production mode

--- server/index.js
+++ server/index.js
@@ -25,8 +25,8 @@ app.use(cookieParser())
 //middlewares
 app.use(express.json());
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4000",
+  // "http://localhost:5173",
+  // "http://localhost:4000",
 	"https://mychatsphere.vercel.app",
 ];
 
