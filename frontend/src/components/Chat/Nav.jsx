37d61e7c8c634158a877312ddece56d23005fdac
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-26 18:47:51 +0530

    Fixed issue in logout onClick

--- frontend/src/components/Chat/Nav.jsx
+++ frontend/src/components/Chat/Nav.jsx
@@ -106,7 +106,7 @@ const Nav = () => {
               </Link>
             </div>
             <Link className="flex justify-center items-center hover:bg-[#02195a]/40	h-[80px]">
-              <div className="flex flex-col justify-center items-center">
+              <div className="flex flex-col justify-center items-center h-full w-full" onClick={logout}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -121,7 +121,7 @@ const Nav = () => {
                     d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
                   />
                 </svg>
-                <button onClick={logout}>Logout</button>
+                <p>Logout</p>
               </div>
             </Link>
           </nav>
