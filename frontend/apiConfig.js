4dfb6190b5756026c8dcd8bc219b7bdf57fae4c3
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-27 13:31:10 +0530

    changes for production mode

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -2,12 +2,15 @@
 let baseUrl;
 let socketUrl;
 
-if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://my-chatsphere.vercel.app";
-  socketUrl = "wss://my-chatsphere.vercel.app";
-} else {
-  baseUrl = "http://localhost:4000";
-  socketUrl = "ws://localhost:4000";
-}
+baseUrl = "https://my-chatsphere.vercel.app";
+socketUrl = "wss://my-chatsphere.vercel.app";
+
+// if (import.meta.env.VITE_NODE_ENV === "production") {
+//   baseUrl = "https://my-chatsphere.vercel.app";
+//   socketUrl = "wss://my-chatsphere.vercel.app";
+// } else {
+//   baseUrl = "http://localhost:4000";
+//   socketUrl = "ws://localhost:4000";
+// }
 
 export { baseUrl, socketUrl };
