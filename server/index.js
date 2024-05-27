1b17ea081c740976321cdb15e236ecc447e46cd4
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-27 20:00:42 +0530

    code refactored

--- server/index.js
+++ server/index.js
@@ -23,26 +23,25 @@ app.use(express.json())
 app.use(cookieParser())
 
 //middlewares
-app.use(express.json());
-const allowedOrigins = [
-  // "http://localhost:5173",
-  // "http://localhost:4000",
-	"https://my-chatsphere.vercel.app",
-];
+// app.use(express.json());
+// const allowedOrigins = [
+//   // "http://localhost:5173",
+//   // "http://localhost:4000",
+// 	"https://my-chatsphere.vercel.app",
+// ];
 
-
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (allowedOrigins.includes(origin) || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	optionsSuccessStatus: 204,
-	credentials: true, // Allow credentials like cookies
-};
+// const corsOptions = {
+// 	origin: (origin, callback) => {
+// 		if (allowedOrigins.includes(origin) || !origin) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error("Not allowed by CORS"));
+// 		}
+// 	},
+// 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// 	optionsSuccessStatus: 204,
+// 	credentials: true, // Allow credentials like cookies
+// };
 // app.use(cors(corsOptions)); //for dev
 app.use(cors());	//for production
 
@@ -54,12 +53,12 @@ const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Application Running on Port ${port}`));
 
 createWebSocketServer(server); 
-app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+// app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/dist/index.html'), (err) => {
-		if (err) {
-			console.error('Error sending file:', err);
-		}
-	});
-});
+// app.get('/*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '../frontend/dist/index.html'), (err) => {
+// 		if (err) {
+// 			console.error('Error sending file:', err);
+// 		}
+// 	});
+// });
