f174bd00576790d2780ffa68d8a7265d0b2bcd2e
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-27 20:46:09 +0530

    enabled cors

--- server/index.js
+++ server/index.js
@@ -24,26 +24,26 @@ app.use(cookieParser())
 
 //middlewares
 // app.use(express.json());
-// const allowedOrigins = [
-//   // "http://localhost:5173",
-//   // "http://localhost:4000",
-// 	"https://my-chatsphere.vercel.app",
-// ];
+const allowedOrigins = [
+  // "http://localhost:5173",
+  // "http://localhost:4000",
+	"https://my-chatsphere.vercel.app",
+];
 
-// const corsOptions = {
-// 	origin: (origin, callback) => {
-// 		if (allowedOrigins.includes(origin) || !origin) {
-// 			callback(null, true);
-// 		} else {
-// 			callback(new Error("Not allowed by CORS"));
-// 		}
-// 	},
-// 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-// 	optionsSuccessStatus: 204,
-// 	credentials: true, // Allow credentials like cookies
-// };
-// app.use(cors(corsOptions)); //for dev
-app.use(cors());	//for production
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	optionsSuccessStatus: 204,
+	credentials: true, // Allow credentials like cookies
+};
+app.use(cors(corsOptions)); //for dev
+// app.use(cors());	//for production
 
 app.use("/api/user", userRoute);
 app.use("/api/avatar", avatarRoute);
