877860ea61f577e980efd74f2e934b7472bd7c78
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-27 19:30:43 +0530

    code refactored

--- index.js
+++ index.js
@@ -17,38 +17,15 @@ dotenv.config({
   path: `./.env`,
 });
 
-//database connection
 connectDB();
-app.use(express.json())
+
 app.use(cookieParser())
-
-//middlewares
 app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:4000",
-];
-
-
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (allowedOrigins.includes(origin) || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	optionsSuccessStatus: 204,
-	credentials: true, // Allow credentials like cookies
-};
-// app.use(cors(corsOptions)); //for dev
-app.use(cors());	//for production
+app.use(cors());
 
 app.use("/api/user", userRoute);
 app.use("/api/avatar", avatarRoute);
-// console.log(process.env.SMTP_USER);
-// console.log(process.env.SMTP_PASS);
+
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Application Running on Port ${port}`));
 
