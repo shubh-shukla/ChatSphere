b867c16e40b489a1c70c9541b53908bf43012612
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-28 18:50:36 +0530

    added dist folder

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,8 +3,8 @@ let baseUrl;
 let socketUrl;
 
 if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://chatsphere-server.vercel.app";
-  socketUrl = "wss://chatsphere-server.vercel.app";
+  baseUrl = "https://my-chat-sphere.vercel.app";
+  socketUrl = "wss://my-chat-sphere.vercel.app";
 } else {
   baseUrl = "http://localhost:4000";
   socketUrl = "ws://localhost:4000";
