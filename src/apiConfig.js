74b9f9f2862793844e52b79f051850a3ddc4a58e
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-28 20:14:08 +0530

    updated base url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,12 +2,12 @@
 let baseUrl;
 let socketUrl;
 
-if (import.meta.env.VITE_NODE_ENV === "production") {
+// if (import.meta.env.VITE_NODE_ENV === "production") {
   baseUrl = "https://my-chat-sphere.vercel.app";
   socketUrl = "wss://my-chat-sphere.vercel.app";
-} else {
-  baseUrl = "http://localhost:4000";
-  socketUrl = "ws://localhost:4000";
-}
+// } else {
+//   baseUrl = "http://localhost:4000";
+//   socketUrl = "ws://localhost:4000";
+// }
 
 export { baseUrl, socketUrl };
