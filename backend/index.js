dd221c5da2710d00ef900ba29c4f8cc3f29c6f29
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-29 08:52:51 +0530

    project setup for render deploymenrt

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import connectDB from "./db/db.js";
+import userRoute from "./routes/userRoute.js";
+import avatarRoute from "./routes/avatarRoute.js";
+import createWebSocketServer from "./wsServer.js";
+
+const app = express();
+
+dotenv.config({
+  path: `./.env`,
+});
+
+//database connection
+connectDB();
+app.use(express.json())
+app.use(cookieParser())
+
+//middlewares
+app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4000",
+	"https://my-chat-sphere.onrender.app",
+];
+
+
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	optionsSuccessStatus: 204,
+	credentials: true, // Allow credentials like cookies
+};
+app.use(cors(corsOptions)); //for dev
+// app.use(cors());	//for production
+
+app.use("/api/user", userRoute);
+app.use("/api/avatar", avatarRoute);
+
+
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => console.log(`Application Running on Port ${port}`));
+
+createWebSocketServer(server); 
--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -2,12 +2,12 @@
 let baseUrl;
 let socketUrl;
 
-if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "https://chatsphere-server.vercel.app";
-  socketUrl = "wss://chatsphere-server.vercel.app";
-} else {
-  baseUrl = "http://localhost:4000";
-  socketUrl = "ws://localhost:4000";
-}
+// if (import.meta.env.VITE_NODE_ENV === "production") {
+  baseUrl = "https://my-chat-sphere.onrender.app";
+  socketUrl = "wss://my-chat-sphere.onrender.app";
+// } else {
+  // baseUrl = "http://localhost:4000";
+  // socketUrl = "ws://localhost:4000";
+// }
 
 export { baseUrl, socketUrl };
