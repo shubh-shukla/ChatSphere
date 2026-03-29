44fdbbf11ef43e859b04d0fe212d2489ece88a9b
shubh-shukla <shukla111shubh@gmail.com>
2026-03-29 14:01:43 +0530

    Update host config for SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async (email, subject, verificationUrl) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
+      host: process.env.SMTP_HOST,
       service: process.env.SERVICE,
       port: 587,
       // secure: Boolean(process.env.SECURE),
